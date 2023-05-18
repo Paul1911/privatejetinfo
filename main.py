f63efc2d63299f00eea7479310c2b3c51795a074
Paul <paul.friedrich@iubh.de>
2023-05-18 23:29:03 +0200

    added initial mail service draft

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from config import config
 import scraper 
 import logging
 logging.basicConfig(level=logging.INFO)
+from email.message import EmailMessage
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 
 user = config['user_02']
 user_pw = config['user_password_02']
@@ -15,4 +21,33 @@ if __name__ == "__main__":
         AirHamburg.login()
         html = AirHamburg.get_table_html()
         df = AirHamburg.html_to_df(html)
-        print(df)
+    print(df)
+
+    email_sender = config['user_mail']
+    email_password = config['user_password_mail']
+    email_receiver = 'paul.friedrich@gmx.net'
+    subject = 'testsubject'
+    
+
+    html = """\
+        <html>
+        <head></head>
+        <body>
+            {0}
+        </body>
+        </html>
+        """.format(df.to_html())
+    part1 = MIMEText(html, 'html')
+
+    em = MIMEMultipart()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    #em.set_content(body)
+    em.attach(part1)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
